fe20f105e9cf3e0fbe58fe8a2860229388097144
Jiek Ruan <ruanjiek2001@gmail.com>
2023-04-28 00:08:25 +0200

    commit exercice

--- exo.py
+++ exo.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import time, numpy, math
+import time, numpy, math, random
 # fonction qui retourne le sin de la courbe 
 def f(x):
     return numpy.exp(-x/10) * numpy.sin(x)
@@ -23,16 +23,40 @@ gradient = numpy.gradient(y_values, x_values)
 slicing = slice(3000, 7000)
 print(y_values[slicing])
 sum_y = sum(y_values[slicing])/len(y_values[slicing])
-print(sum_y)
+print("la moyenne :",sum_y)
 # ecartype
 ecart_type = numpy.std(y_values[slicing])
-print(ecart_type)
-# Lorsque la dérivé f(x) est null
+print("l'ecart-type :", ecart_type)
+# Lorsque la dérivé f(x) = 0
+def f_prime(x) :
+    return ((numpy.exp(-x/10.0) + numpy.cos(x)) * ((-0.1 * numpy.exp(-x/10.0)) + numpy.sin(x)))
+def dichotomie(f, a, b):
+    pas= 1e-6
+    while (b - a) > pas:
+        m = (a + b) / 2
+        if f(a) * f(m) < 0:
+            b = m
+        else:
+            a = m
+    return (a + b) / 2
 
+print("Valeur la plus proche entre l'intervalle[0:3] :",dichotomie(f_prime , 0 , 3))
+print("Valeur la plus proche entre l'intervalle[3:6] :",dichotomie(f_prime , 3 , 6))
+print("Valeur la plus proche entre l'intervalle[6:10] :",dichotomie(f_prime , 6 , 10))
 
-
-
+# methode de monte carlo
+def monte_carlo():
+    point = random.randint(1,100)
+    point_2 = random.randint(1,100)
+    if(point == point_2):
+        state = True
+    else:
+        state = False
+    return state
+num_simulations = 10000
+max_points = 1000
 plt.figure(figsize=(10,6))
 plt.plot(x_values, y_values)
 plt.plot(x_values, gradient)
+
 plt.show()
